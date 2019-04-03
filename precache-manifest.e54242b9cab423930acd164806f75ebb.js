cadc47be221011306c4f899aea8e9664a9bd4f63
VladimirZornic <vladimir.zornic@outlook.com>
2019-04-03 18:42:23 +0200

    Updates

---
+++ precache-manifest.e54242b9cab423930acd164806f75ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad5e683a7db7b789bdef",
+    "url": "/rjs/static/js/runtime~main.08b82926.js"
+  },
+  {
+    "revision": "60527fddfc8b7e9e186e",
+    "url": "/rjs/static/js/main.c11d670b.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rjs/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "60527fddfc8b7e9e186e",
+    "url": "/rjs/static/css/main.c89ba000.chunk.css"
+  },
+  {
+    "revision": "646343f0105f80efb052829e4314ca7c",
+    "url": "/rjs/index.html"
+  }
+];
